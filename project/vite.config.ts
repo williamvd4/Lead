35d771bbb949fa37183023261cd0287141f7884b
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-11-21 09:36:24 -0500

    Trying again

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import history from 'connect-history-api-fallback';
-import type { Connect } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'history-fallback',
-      configureServer(server) {
-        server.middlewares.use(
-          history() as Connect.NextHandleFunction
-        );
-      },
-    },
-  ],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
