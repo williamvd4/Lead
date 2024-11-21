4690d71b3fa31c7a451492f4b5656b7ef29d6235
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-11-21 09:16:26 -0500

    might not work.  Rebuilt but errors in config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import history from 'connect-history-api-fallback';
-import { Plugin } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  configureServer: (server) => {
-    server.middlewares.use(
-      history({
-        disableDotRule: true,
-        verbose: true,
-      })
-    );
   },
+  server: {
+    middlewareMode: true,
+    configureServer: (server: any) => {
+      server.middlewares.use(
+        history({
+          disableDotRule: true,
+          verbose: true,
+        })
+      );
+    },
   },
 });
