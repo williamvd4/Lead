3765ff850bc44d20c7386fd5a217806978f077c1
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-11-21 09:13:36 -0500

    probalby wont work.  can go back to last for working code

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import history from 'connect-history-api-fallback';
+import { Plugin } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  configureServer: (server) => {
+    server.middlewares.use(
+      history({
+        disableDotRule: true,
+        verbose: true,
+      })
+    );
   },
-  server: {
-    middlewareMode: true,
-  }
+  },
 });
