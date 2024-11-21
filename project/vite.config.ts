0381ea6b63a438ac25fb6b3b1bad8c6bc833755e
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-11-21 08:32:39 -0500

    render.yaml created and updated vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-base: '/project/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    middlewareMode: true,
+  }
 });
