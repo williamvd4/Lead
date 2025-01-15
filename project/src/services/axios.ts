a4c2a73cb78708724d373e7df3eddbcb7ae818f8
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2025-01-15 13:32:12 -0500

    Update API URL for production and adjust Vite configuration: change base path and exclude axios from Rollup build.

--- project/src/services/axios.ts
+++ project/src/services/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/';  // Ensure this is correct
+const API_URL = 'https://api-2f70.onrender.com';  // Ensure this is correct
 
 export const getProducts = async () => {
     const response = await axios.get(`${API_URL}products/`);
