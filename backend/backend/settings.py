66b31f62f0755ab9fb26633937e5b89d135ef3b5
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2025-01-21 07:04:03 -0500

    Remove outdated budget transfer PDF and update MEDIA_ROOT for improved media file handling

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Use the Render disk mount path for storing media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT =  'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT =  'staticfiles'
 
