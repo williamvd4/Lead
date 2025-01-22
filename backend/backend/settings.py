8cff0cd183edf6fafc9176c4cf64a5c45164ff9b
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2025-01-22 08:12:12 -0500

    Remove initial migration file and add secret key to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,6 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'j_grh1_jifo8a%a*o$^sk2kk66byq*i&am5bcx3uiw+0(u%y&!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = [
