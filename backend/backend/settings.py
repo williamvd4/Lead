34e73faf06a3a4b61f919600759aeb923b00b3db
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2025-01-22 08:00:41 -0500

    Refactor HomeCarouselItem model: add admin_title field and update link_page choices; modify title and description fields to allow blank values

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,38 +1,16 @@
-
-import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Use the Render disk mount path for storing media files
-
-STATIC_URL = '/static/'
-STATIC_ROOT =  'staticfiles'
-MEDIA_URL = 'https://leadback.onrender.com/media/'
-MEDIA_ROOT = os.path.join('/mnt/data', 'media')
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gl%0g@o071egzz5%2hd0p__k)8f$o+-5n^a21$7)p332$_3ug*"
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = [
-    "localhost",
     "127.0.0.1",
-    "localhost:5173",
-    "leadfront.onrender.com",
-    "leadback.onrender.com",
-    ".onrender.com",
+    "localhost"
 ]
-
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -41,53 +19,23 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "api",
     "corsheaders",
-    "storages",
+    "api",
 ]
-
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    
 ]
-
-
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_USE_FINDERS = True
-WHITENOISE_ROOT = MEDIA_ROOT
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-FILE_UPLOAD_PERMISSIONS = 0o644
-FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o755
-
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://leadfront.onrender.com",
-    "https://leadback.onrender.com",    
 ]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://leadfront.onrender.com",
-    'https://leadback.onrender.com'
-]
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny' # Or appropriate permissions
-    ]
-}
-
 ROOT_URLCONF = "backend.urls"
-
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -103,28 +51,17 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = "backend.wsgi.application"
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "jang",
-        "USER": "jang_user",
-        "PASSWORD": "ckpbTS6dgp9hD4M8iSGVykd4V4svbslE",
-        "HOST": "dpg-cu7pc7bqf0us73e76oa0-a", 
-        "URL": "postgresql://jang_user:ckpbTS6dgp9hD4M8iSGVykd4V4svbslE@dpg-cu7pc7bqf0us73e76oa0-a/jang",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -139,22 +76,15 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
